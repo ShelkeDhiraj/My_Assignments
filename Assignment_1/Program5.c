dc4f1ee104af60648bc37b498b9270293501fbe8
ShelkeDhiraj <147151803+ShelkeDhiraj@users.noreply.github.com>
2023-10-06 18:27:32 +0530

    Add files via upload

---
+++ Assignment_1/Program5.c
@@ -0,0 +1,24 @@
+// Program Statment :  Accept one number from user and print that number of * on screen
+
+#include <stdio.h>
+
+void Accept(int iNo)
+{
+    int iCnt = 0;
+
+    for (iCnt = 0; iCnt < iNo; iCnt++)
+    {
+        printf("*");
+        
+    }
+}
+
+int main()
+{
+    auto int iValue = 0;
+    iValue = 5;
+
+    Accept(iValue);
+
+    return 0;
+}
